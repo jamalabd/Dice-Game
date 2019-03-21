bfe1bd1cede09230d7eee776f870f10b3244cf2c
Jamal Abdul-majid <jamalabdul-majid@Jamals-MacBook-Pro.local>
2019-03-21 01:33:39 -0400

    need to update total score

--- starter/app.js
+++ starter/app.js
@@ -8,17 +8,58 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-let scores, roundScore, activePlayer, dice;
-scores = [0,0]
+var scores, roundScore, activePlayer;
+scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
-dice = Math.floor(Math.random() * 7);
-//console.log(dice);
+activePlayer = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// console.log(dice)
 // document.querySelector('#current-' + activePlayer).innerHTML  = '<em>' + dice + '</em>'
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        if (dice !== 1) {
+         roundScore += dice;
+         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+        }else {
+           activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+           roundScore = 0;
+           document.querySelector('#current-0').textContent = '0';
+           document.querySelector('#current-1').textContent = '0';
+
+
+           document.querySelector('.player-0-panel').classList.toggle('active');
+           document.querySelector('.player-1-panel').classList.toggle('active');
+
+           document.querySelector('.dice').style.display = 'none';
+
+        }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+});
